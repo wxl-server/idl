e3d0e6739d5d91908c0742c3bf4a19eb11b41889
nas <2659305925@qq.com>
2025-02-19 10:49:27 +0800

    update

--- rpc_idl/fabric_ebl.thrift
+++ rpc_idl/fabric_ebl.thrift
@@ -22,13 +22,33 @@ struct LoginResp {
     1: required string token
 }
 
+struct GetUserInfoReq {
+}
+
+struct GetUserInfoResp {
+    1: required string user_id
+    2: required string user_email
+    3: required string user_name
+    4: required UserType user_type
+    5: required string company_id
+    6: required string company_name
+    7: required string company_code
+    8: required CompanyType company_type
+}
+
 service FabricEbl{
     CreateCompanyResp CreateCompany(1: CreateCompanyReq req)
     LoginResp Login(1: LoginReq req)
+    GetUserInfoResp GetUserInfo(1: GetUserInfoReq req)
 }
 
 enum CompanyType {
     Ship = 1;
     Bank = 2;
     Common = 3;
+}
+
+enum UserType {
+    Admin = 1;
+    User = 2;
 }
