fbfbd7b2390bda5a159d9859a0d4942fd0ae629e
nas <2659305925@qq.com>
2025-02-22 14:39:31 +0800

    update

--- rpc_idl/fabric_ebl.thrift
+++ rpc_idl/fabric_ebl.thrift
@@ -118,6 +118,15 @@ struct QueryEblListResp {
     3: required string bookmark         // 返回的分页标记，用于继续分页查询
 }
 
+struct SubmitEblReq {
+    1: required string token            // 身份验证Token
+    2: required i64 eblNo            // Ebl ID
+}
+
+struct SubmitEblResp {
+    1: required i64 id
+}
+
 service FabricEbl{
     CreateCompanyResp CreateCompany(1: CreateCompanyReq req)
     LoginResp Login(1: LoginReq req)
@@ -126,6 +135,7 @@ service FabricEbl{
     CreateEblResp CreateEbl(1: CreateEblReq req)
     QueryAllEblListResp QueryAllEblList(1: QueryAllEblListReq req)
     QueryEblListResp QueryEblList(1: QueryEblListReq req)
+    SubmitEblResp SubmitEbl(1: SubmitEblReq req)
 }
 
 struct Company {
