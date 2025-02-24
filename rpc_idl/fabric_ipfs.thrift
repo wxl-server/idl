7e7b2a9289be5d00aa2ad4eddb8c05f1e89c6dc3
nas <2659305925@qq.com>
2025-02-24 18:16:40 +0800

    update

--- rpc_idl/fabric_ipfs.thrift
+++ rpc_idl/fabric_ipfs.thrift
@@ -10,44 +10,31 @@ struct CreateEblDocxResp {
 
 struct CreateEblDocx {
     1: required string eblNo
-    2: required string originCompanyID
-    3: required string originCompanyName
-    4: required string shipperCompanyID
-    5: required string shipperCompanyName
-    6: required string consigneeCompanyID
-    7: required string consigneeCompanyName
-    8: required string notifyPartyCompanyID
-    9: required string notifyPartyCompanyName
-    10: required string placeOfReceipt
-    11: required string oceanVessel
-    12: required string portOfLoading
-    13: required string portOfDescharge
-    14: required string placeOfDestination
-    15: required string placeOfDelivery
-    16: required string shippingMarkes
-    17: required double quantityOfPackages
-    18: required string kindOfPackagesGW
-    19: required string kindOfPackagesM
-    20: required string descriptionOfGoods
-    21: required double grossWeight
-    22: required double measurement
-    23: required string freightAndCharges
-    24: required string placeOfIssue
-    25: required i64 dateOfIssue
-    26: required string deliveryAgent
-    27: required i64 shippedOnBoard
-    28: required i64 numOfEbl
-    29: required i64 dateOfIssueDeadline
-    30: required string status
-    31: required string file
-    32: required list<string> contractFiles
-    33: required list<string> invoiceFiles
-    34: required string transferCompanyID
-    35: required string transferCompanyName
-    36: required string companyID
-    37: required string companyName
+    2: required string originCompanyName
+    3: required string shipperCompanyName
+    4: required string consigneeCompanyName
+    5: required string notifyPartyCompanyName
+    6: required string placeOfReceipt
+    7: required string oceanVessel
+    8: required string portOfLoading
+    9: required string portOfDescharge
+    10: required string placeOfDestination
+    11: required string placeOfDelivery
+    12: required string shippingMarkes
+    13: required string quantityOfPackages
+    14: required string kindOfPackagesGW
+    15: required string kindOfPackagesM
+    16: required string descriptionOfGoods
+    17: required string grossWeight
+    18: required string measurement
+    19: required string freightAndCharges
+    20: required string placeOfIssue
+    21: required string dateOfIssue
+    22: required string deliveryAgent
+    23: required string shippedOnBoard
+    24: required string numOfEbl
+    25: required string dateOfIssueDeadline
 }
-
 
 service FabricIpfs{
     CreateEblDocxResp CreateEblDocx(1: CreateEblDocxReq req)
