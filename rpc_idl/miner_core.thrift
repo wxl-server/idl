66135db31c5d2d70fae3b8aa341694d20ac1cabf
Wang Xinglong <wangxinglong.123@bytedance.com>
2025-03-02 13:37:18 +0800

    update

--- rpc_idl/miner_core.thrift
+++ rpc_idl/miner_core.thrift
@@ -20,8 +20,28 @@ struct QueryJobListResp {
     2: required i64 total
 }
 
+struct SignUpReq {
+    1: required string email
+    2: required string password
+}
+
+struct SignUpResp {
+    1: required i64 id
+}
+
+struct LoginReq {
+    1: required string email
+    2: required string password
+}
+
+struct LoginResp {
+    1: required string token
+}
+
 service MinerCore{
     QueryJobListResp QueryJobList(1: QueryJobListReq req)
+    SignUpResp SignUp(1: SignUpReq req)
+    LoginResp Login(1: LoginReq req)
 }
 
 enum Order {
