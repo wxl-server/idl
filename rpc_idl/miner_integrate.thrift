1bf986c079ea8ca4bd3965c5089bfc1abd370322
qcq1 <1715142548@qq.com>
2025-04-13 02:24:03 +0800

    update

--- rpc_idl/miner_integrate.thrift
+++ rpc_idl/miner_integrate.thrift
@@ -1,4 +1,15 @@
 namespace go miner_integrate
 
+struct UpdateMockProducerQpsReq {
+    1: optional i64 product_base_info_qps
+    2: optional i64 product_shop_info_qps
+    3: optional i64 product_logo_model_info_qps
+    4: optional i64 product_image_model_info_qps
+}
+
+struct UpdateMockProducerQpsResp {
+}
+
 service MinerIntegrate {
+    UpdateMockProducerQpsResp UpdateMockProducerQps(1: UpdateMockProducerQpsReq req)
 }
