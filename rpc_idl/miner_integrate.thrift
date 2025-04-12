682380df8010a7ee81154ce26d4a37d297b1fe05
qcq1 <1715142548@qq.com>
2025-04-13 04:48:30 +0800

    update

--- rpc_idl/miner_integrate.thrift
+++ rpc_idl/miner_integrate.thrift
@@ -5,11 +5,9 @@ struct UpdateMockProducerQpsReq {
     2: optional i64 product_shop_info_qps
     3: optional i64 product_logo_model_info_qps
     4: optional i64 product_image_model_info_qps
-    5: optional i64 test
 }
 
 struct UpdateMockProducerQpsResp {
-
 }
 
 service MinerIntegrate {
